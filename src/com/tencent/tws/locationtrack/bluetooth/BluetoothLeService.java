da721cd3e95de13e535563e76fa53bb24ca6dee2
microzhang <1010304468@qq.com>
2015-10-09 20:18:41 +0800

    add bluetooth

---
+++ src/com/tencent/tws/locationtrack/bluetooth/BluetoothLeService.java
@@ -0,0 +1,283 @@
+/*
+ * Copyright (C) 2013 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.tencent.tws.locationtrack.bluetooth;
+
+import android.app.Service;
+import android.bluetooth.*;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+import android.util.Log;
+
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * Service for managing connection and data communication with a GATT server hosted on a
+ * given Bluetooth LE device.
+ */
+public class BluetoothLeService extends Service {
+	private final static String TAG = BluetoothLeService.class.getSimpleName();
+
+	private BluetoothManager mBluetoothManager;
+	private BluetoothAdapter mBluetoothAdapter;
+	private String mBluetoothDeviceAddress;
+	private BluetoothGatt mBluetoothGatt;
+	private int mConnectionState = STATE_DISCONNECTED;
+
+	private static final int STATE_DISCONNECTED = 0;
+	private static final int STATE_CONNECTING = 1;
+	private static final int STATE_CONNECTED = 2;
+
+	public final static String ACTION_GATT_CONNECTED = "com.tencent.tws.locationtrack.ACTION_GATT_CONNECTEDD";
+	public final static String ACTION_GATT_DISCONNECTED = "com.tencent.tws.locationtrack.ACTION_GATT_DISCONNECTED";
+	public final static String ACTION_GATT_SERVICES_DISCOVERED = "com.tencent.tws.locationtrack.ACTION_GATT_SERVICES_DISCOVERED";
+	public final static String ACTION_DATA_AVAILABLE = "com.tencent.tws.locationtrack.ACTION_DATA_AVAILABLE";
+	public final static String EXTRA_DATA = "com.tencent.tws.locationtrack.EXTRA_DATA";
+
+	public final static UUID UUID_HEART_RATE_MEASUREMENT = UUID.fromString(SampleGattAttributes.HEART_RATE_MEASUREMENT);
+
+	// 连接状态回调
+	private final BluetoothGattCallback mGattCallback = new BluetoothGattCallback() {
+		@Override
+		public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
+			if (newState == BluetoothProfile.STATE_CONNECTED) {
+				mConnectionState = STATE_CONNECTED;
+				broadcastUpdate(ACTION_GATT_CONNECTED);
+				Log.i(TAG, "Connected to GATT server.");
+			} else if (newState == BluetoothProfile.STATE_DISCONNECTED) {
+				mConnectionState = STATE_DISCONNECTED;
+				Log.i(TAG, "Disconnected from GATT server.");
+				broadcastUpdate(ACTION_GATT_DISCONNECTED);
+			}
+		}
+
+		@Override
+		public void onServicesDiscovered(BluetoothGatt gatt, int status) {
+			if (status == BluetoothGatt.GATT_SUCCESS) {
+				broadcastUpdate(ACTION_GATT_SERVICES_DISCOVERED);
+			} else {
+				Log.w(TAG, "onServicesDiscovered received: " + status);
+			}
+		}
+
+		@Override
+		public void onCharacteristicRead(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status) {
+			if (status == BluetoothGatt.GATT_SUCCESS) {
+			}
+		}
+
+		@Override
+		public void onCharacteristicChanged(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic) {
+		}
+	};
+
+	private void broadcastUpdate(final String action) {
+		final Intent intent = new Intent(action);
+		sendBroadcast(intent);
+	}
+
+	public class LocalBinder extends Binder {
+		BluetoothLeService getService() {
+			return BluetoothLeService.this;
+		}
+	}
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		return mBinder;
+	}
+
+	@Override
+	public boolean onUnbind(Intent intent) {
+		close();
+		return super.onUnbind(intent);
+	}
+
+	private final IBinder mBinder = new LocalBinder();
+
+	/**
+	 * Initializes a reference to the local Bluetooth adapter.
+	 *
+	 * @return Return true if the initialization is successful.
+	 */
+	public boolean initialize() {
+		// For API level 18 and above, get a reference to BluetoothAdapter through
+		// BluetoothManager.
+		if (mBluetoothManager == null) {
+			mBluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+			if (mBluetoothManager == null) {
+				Log.e(TAG, "Unable to initialize BluetoothManager.");
+				return false;
+			}
+		}
+
+		mBluetoothAdapter = mBluetoothManager.getAdapter();
+		if (mBluetoothAdapter == null) {
+			Log.e(TAG, "Unable to obtain a BluetoothAdapter.");
+			return false;
+		}
+
+		return true;
+	}
+
+
+	@Override
+	public int onStartCommand(Intent intent, int flags, int startId) {
+		Log.i(TAG, "onStartCommand");
+
+		initialize();
+		//开始扫描设备
+		mBluetoothAdapter.startLeScan(mLeScanCallback);
+
+		String address = getSharedPreferences("test", Context.MODE_PRIVATE).getString(DeviceControlActivity.EXTRAS_DEVICE_ADDRESS, "");
+		if (address != null && !address.equals("")) {
+			connect(address);
+		}
+
+		return super.onStartCommand(intent, flags, startId);
+	}
+
+	private BluetoothAdapter.LeScanCallback mLeScanCallback = new BluetoothAdapter.LeScanCallback() {
+
+		@Override
+		public void onLeScan(final BluetoothDevice device, int rssi, byte[] scanRecord) {
+			String address = getSharedPreferences("test", Context.MODE_PRIVATE).getString(DeviceControlActivity.EXTRAS_DEVICE_ADDRESS, "");
+			if (device.getAddress().equals(address)) {
+				Log.i(TAG, "找到设备,停止扫描并开始连接");
+				mBluetoothAdapter.stopLeScan(mLeScanCallback);
+				connect(address);
+			}
+		}
+	};
+
+	/**
+	 * Connects to the GATT server hosted on the Bluetooth LE device.
+	 *
+	 * @param address The device address of the destination device.
+	 * @return Return true if the connection is initiated successfully. The connection result
+	 * is reported asynchronously through the
+	 * {@code BluetoothGattCallback#onConnectionStateChange(android.com.tencent.tws.locationtrack.bluetooth.BluetoothGatt, int, int)}
+	 * callback.
+	 */
+
+	public boolean connect(final String address) {
+		if (mBluetoothAdapter == null || address == null) {
+			Log.w(TAG, "connect BluetoothAdapter not initialized or unspecified address.");
+			return false;
+		}
+
+		// Previously connected device.  Try to reconnect.
+		if (mBluetoothDeviceAddress != null && address.equals(mBluetoothDeviceAddress) && mBluetoothGatt != null) {
+			Log.d(TAG, "Trying to use an existing mBluetoothGatt for connection.");
+			if (mBluetoothGatt.connect()) {
+				mConnectionState = STATE_CONNECTING;
+				return true;
+			} else {
+				return false;
+			}
+		}
+
+		final BluetoothDevice device = mBluetoothAdapter.getRemoteDevice(address);
+		if (device == null) {
+			Log.w(TAG, "Device not found.  Unable to connect.");
+			return false;
+		}
+		// We want to directly connect to the device, so we are setting the autoConnect
+		// parameter to false.
+		mBluetoothGatt = device.connectGatt(this, false, mGattCallback);
+		Log.d(TAG, "Trying to create a new connection.");
+		mBluetoothDeviceAddress = address;
+		mConnectionState = STATE_CONNECTING;
+		return true;
+	}
+
+	/**
+	 * Disconnects an existing connection or cancel a pending connection. The disconnection result
+	 * is reported asynchronously through the
+	 * {@code BluetoothGattCallback#onConnectionStateChange(android.com.tencent.tws.locationtrack.bluetooth.BluetoothGatt, int, int)}
+	 * callback.
+	 */
+	public void disconnect() {
+		if (mBluetoothAdapter == null || mBluetoothGatt == null) {
+			Log.w(TAG, " disconnect BluetoothAdapter not initialized");
+			return;
+		}
+		mBluetoothGatt.disconnect();
+	}
+
+	/**
+	 * After using a given BLE device, the app must call this method to ensure resources are
+	 * released properly.
+	 */
+	public void close() {
+		if (mBluetoothGatt == null) {
+			return;
+		}
+		mBluetoothGatt.close();
+		mBluetoothGatt = null;
+	}
+
+	/**
+	 * Request a read on a given {@code BluetoothGattCharacteristic}. The read result is reported
+	 * asynchronously through the {@code BluetoothGattCallback#onCharacteristicRead(android.com.tencent.tws.locationtrack.bluetooth.BluetoothGatt, android.com.tencent.tws.locationtrack.bluetooth.BluetoothGattCharacteristic, int)}
+	 * callback.
+	 *
+	 * @param characteristic The characteristic to read from.
+	 */
+	public void readCharacteristic(BluetoothGattCharacteristic characteristic) {
+		if (mBluetoothAdapter == null || mBluetoothGatt == null) {
+			Log.w(TAG, "readCharacteristic BluetoothAdapter not initialized");
+			return;
+		}
+		mBluetoothGatt.readCharacteristic(characteristic);
+	}
+
+	/**
+	 * Enables or disables notification on a give characteristic.
+	 *
+	 * @param characteristic Characteristic to act on.
+	 * @param enabled        If true, enable notification.  False otherwise.
+	 */
+	public void setCharacteristicNotification(BluetoothGattCharacteristic characteristic, boolean enabled) {
+		if (mBluetoothAdapter == null || mBluetoothGatt == null) {
+			Log.w(TAG, "setCharacteristicNotification BluetoothAdapter not initialized");
+			return;
+		}
+		mBluetoothGatt.setCharacteristicNotification(characteristic, enabled);
+
+		// This is specific to Heart Rate Measurement.
+		if (UUID_HEART_RATE_MEASUREMENT.equals(characteristic.getUuid())) {
+			BluetoothGattDescriptor descriptor = characteristic.getDescriptor(UUID.fromString(SampleGattAttributes.CLIENT_CHARACTERISTIC_CONFIG));
+			descriptor.setValue(BluetoothGattDescriptor.ENABLE_NOTIFICATION_VALUE);
+			mBluetoothGatt.writeDescriptor(descriptor);
+		}
+	}
+
+	/**
+	 * Retrieves a list of supported GATT services on the connected device. This should be
+	 * invoked only after {@code BluetoothGatt#discoverServices()} completes successfully.
+	 *
+	 * @return A {@code List} of supported services.
+	 */
+	public List<BluetoothGattService> getSupportedGattServices() {
+		if (mBluetoothGatt == null) return null;
+
+		return mBluetoothGatt.getServices();
+	}
+}
---
+++ src/com/tencent/tws/locationtrack/bluetooth/DeviceScanActivity.java
@@ -0,0 +1,284 @@
+/*
+ * Copyright (C) 2013 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.tencent.tws.locationtrack.bluetooth;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.*;
+import com.tencent.tws.locationtrack.R;
+import com.tencent.tws.widget.BaseActivity;
+
+import java.util.ArrayList;
+
+/**
+ * Activity for scanning and displaying available Bluetooth LE devices.
+ */
+public class DeviceScanActivity extends BaseActivity implements AdapterView.OnItemClickListener {
+	private LeDeviceListAdapter mLeDeviceListAdapter;
+	private BluetoothAdapter mBluetoothAdapter;
+	private boolean mScanning;
+	private Handler mHandler;
+	private ListView listView;
+	private TextView name;
+	private TextView address;
+	private static TextView state;
+	private Button scan;
+	private static final int REQUEST_ENABLE_BT = 1;
+	// Stops scanning after 10 seconds.
+	private static final long SCAN_PERIOD = 10000;
+	SharedPreferences sharedPreferences;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main_list);
+		mHandler = new Handler();
+
+		listView = (ListView) findViewById(R.id.my_list);
+		name = (TextView) findViewById(R.id.name);
+		address = (TextView) findViewById(R.id.address);
+		state = (TextView) findViewById(R.id.state);
+
+		scan = (Button) findViewById(R.id.scan);
+		scan.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				mLeDeviceListAdapter.clear();
+				mLeDeviceListAdapter.notifyDataSetChanged();
+
+				scanLeDevice(false);
+				mHandler.postDelayed(new Runnable() {
+					@Override
+					public void run() {
+						scanLeDevice(true);
+					}
+				}, 300);
+
+			}
+		});
+
+
+		if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
+			Toast.makeText(this, R.string.ble_not_supported, Toast.LENGTH_SHORT).show();
+			finish();
+		}
+		final BluetoothManager bluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+		mBluetoothAdapter = bluetoothManager.getAdapter();
+
+		if (mBluetoothAdapter == null) {
+			Toast.makeText(this, R.string.error_bluetooth_not_supported, Toast.LENGTH_SHORT).show();
+			finish();
+			return;
+		}
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		sharedPreferences = getSharedPreferences("test", Context.MODE_PRIVATE);
+		if (!mBluetoothAdapter.isEnabled()) {
+			if (!mBluetoothAdapter.isEnabled()) {
+				Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+				startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+			}
+		}
+
+		mLeDeviceListAdapter = new LeDeviceListAdapter();
+		listView.setAdapter(mLeDeviceListAdapter);
+		listView.setOnItemClickListener(this);
+
+		scanLeDevice(true);
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		scanLeDevice(false);
+		mLeDeviceListAdapter.clear();
+	}
+
+	private void scanLeDevice(final boolean enable) {
+		if (enable) {
+			// Stops scanning after a pre-defined scan period.
+			mHandler.postDelayed(new Runnable() {
+				@Override
+				public void run() {
+					mScanning = false;
+					mBluetoothAdapter.stopLeScan(mLeScanCallback);
+					invalidateOptionsMenu();
+				}
+			}, SCAN_PERIOD);
+
+			mScanning = true;
+			mBluetoothAdapter.startLeScan(mLeScanCallback);
+		} else {
+			mScanning = false;
+			mBluetoothAdapter.stopLeScan(mLeScanCallback);
+		}
+		invalidateOptionsMenu();
+	}
+
+	@Override
+	public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+		final BluetoothDevice device = mLeDeviceListAdapter.getDevice(position);
+		if (device == null) return;
+
+		//点击后保存数据
+		if (sharedPreferences == null || !DeviceControlActivity.EXTRAS_DEVICE_ADDRESS.equals(device.getAddress())) {
+			SharedPreferences.Editor editor = sharedPreferences.edit();//获取编辑器
+			editor.putString(DeviceControlActivity.EXTRAS_DEVICE_NAME, device.getName());
+			editor.putString(DeviceControlActivity.EXTRAS_DEVICE_ADDRESS, device.getAddress());
+			editor.commit();
+		}
+
+		if (mScanning) {
+			mBluetoothAdapter.stopLeScan(mLeScanCallback);
+			mScanning = false;
+		}
+
+		name.setText(sharedPreferences.getString(DeviceControlActivity.EXTRAS_DEVICE_NAME, ""));
+		address.setText(sharedPreferences.getString(DeviceControlActivity.EXTRAS_DEVICE_ADDRESS, ""));
+
+		//启动service去连接设备
+		final Intent intent = new Intent(DeviceScanActivity.this, BluetoothLeService.class);
+		startService(intent);
+
+	}
+
+	// Adapter for holding devices found through scanning.
+	private class LeDeviceListAdapter extends BaseAdapter {
+		private ArrayList<BluetoothDevice> mLeDevices;
+		private LayoutInflater mInflator;
+
+		public LeDeviceListAdapter() {
+			super();
+			mLeDevices = new ArrayList<BluetoothDevice>();
+			mInflator = DeviceScanActivity.this.getLayoutInflater();
+		}
+
+		public void addDevice(BluetoothDevice device) {
+			if (!mLeDevices.contains(device)) {
+				mLeDevices.add(device);
+
+				//找到存在过的地址了，就自动请求连接
+				if (device.getAddress().equals(sharedPreferences.getString(DeviceControlActivity.EXTRAS_DEVICE_ADDRESS, ""))) {
+					Toast.makeText(getApplicationContext(), "进入自动连接", Toast.LENGTH_SHORT).show();
+
+					if (mScanning) {
+						mBluetoothAdapter.stopLeScan(mLeScanCallback);
+						mScanning = false;
+					}
+
+					name.setText(sharedPreferences.getString(DeviceControlActivity.EXTRAS_DEVICE_NAME, ""));
+					address.setText(sharedPreferences.getString(DeviceControlActivity.EXTRAS_DEVICE_ADDRESS, ""));
+
+					final Intent intent = new Intent(DeviceScanActivity.this, BluetoothLeService.class);
+					startService(intent);
+				}
+			}
+		}
+
+		public BluetoothDevice getDevice(int position) {
+			return mLeDevices.get(position);
+		}
+
+		public void clear() {
+			mLeDevices.clear();
+		}
+
+		@Override
+		public int getCount() {
+			return mLeDevices.size();
+		}
+
+		@Override
+		public Object getItem(int i) {
+			return mLeDevices.get(i);
+		}
+
+		@Override
+		public long getItemId(int i) {
+			return i;
+		}
+
+		@Override
+		public View getView(int i, View view, ViewGroup viewGroup) {
+			ViewHolder viewHolder;
+			// General ListView optimization code.
+			if (view == null) {
+				view = mInflator.inflate(R.layout.listitem_device, null);
+				viewHolder = new ViewHolder();
+				viewHolder.deviceAddress = (TextView) view.findViewById(R.id.device_address);
+				viewHolder.deviceName = (TextView) view.findViewById(R.id.device_name);
+				view.setTag(viewHolder);
+			} else {
+				viewHolder = (ViewHolder) view.getTag();
+			}
+
+			BluetoothDevice device = mLeDevices.get(i);
+			final String deviceName = device.getName();
+			if (deviceName != null && deviceName.length() > 0) viewHolder.deviceName.setText(deviceName);
+			else viewHolder.deviceName.setText(R.string.unknown_device);
+			viewHolder.deviceAddress.setText(device.getAddress());
+			return view;
+		}
+	}
+
+	// Device scan callback.
+	private BluetoothAdapter.LeScanCallback mLeScanCallback = new BluetoothAdapter.LeScanCallback() {
+
+		@Override
+		public void onLeScan(final BluetoothDevice device, int rssi, byte[] scanRecord) {
+			runOnUiThread(new Runnable() {
+				@Override
+				public void run() {
+					mLeDeviceListAdapter.addDevice(device);
+					mLeDeviceListAdapter.notifyDataSetChanged();
+				}
+			});
+		}
+	};
+
+	public static class ConnRec extends BroadcastReceiver {
+
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			String action = intent.getAction();
+			if (action.equals(BluetoothLeService.ACTION_GATT_CONNECTED)) {
+				if (state != null) state.setText("连接成功");
+			} else if (action.equals(BluetoothLeService.ACTION_GATT_DISCONNECTED)) {
+				if (state != null) state.setText("连接断开");
+			}
+		}
+	}
+
+	static class ViewHolder {
+		TextView deviceName;
+		TextView deviceAddress;
+	}
+}
